f88d1f726eb2266f7acede6706f18ce719f7c175
Yadunand G Kamath <111438964+YadunandKamath@users.noreply.github.com>
2023-08-21 16:47:07 +0530

    Add files via upload

---
+++ java-projects/spring-boot-3-course/spring-actuator-demo/spring-actuator.demo/src/test/java/com/overview/springboot/springactuator/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overview.springboot.springactuator.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
